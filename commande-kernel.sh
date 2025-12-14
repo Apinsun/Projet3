0eee40fa49dc19e7c41a076b0c22348c3685646c
Apinsun <arnaudpinsun@gmail.com>
2025-12-14 16:31:05 +0100

    ajout de la librairie ydata-profiling, génère un .html permettant d'explorer nos données, voir les corrélations, les colonnes ayant principalement une même valeur, etc. c'est top

--- commande-kernel.sh
+++ commande-kernel.sh
@@ -1,1 +1,3 @@
 poetry run python -m ipykernel install --user --name=mon-projet-p3 --display-name "Python (Mon Projet Poetry)"
+
+#source /home/arnaud/.cache/pypoetry/virtualenvs/projet-3-yGtptAP3-py3.13/bin/activate
